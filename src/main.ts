58dda83712f98a39ad5eb6a635126df3ab9841c6
Tonya Mazakova <apmazakova@gmail.com>
2023-02-07 00:36:37 +0300

    - load api.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
@@ -10,14 +14,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const options= new DocumentBuilder()
-    .setTitle('Home Library Service')
-    .setDescription('Home music library service')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('doc', app, document);
+  const document = await readFile('./doc/api.yaml', 'utf-8');
+  SwaggerModule.setup('doc', app, parse(document));
 
   await app.listen(PORT);
 }
